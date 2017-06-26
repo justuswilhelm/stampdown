8df51bdab91de88eab6286668945b7ccaefc1cae
Justus <mail@justusperlwitz.com>
2017-06-26 15:26:05 +0900

    Settings/Prod: Force SSL

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -6,3 +6,6 @@ from . import *  # noqa: F401, F403
 
 ALLOWED_HOSTS = environ['ALLOWED_HOSTS'].split(',')
 SECRET_KEY = environ['SECRET_KEY']
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
